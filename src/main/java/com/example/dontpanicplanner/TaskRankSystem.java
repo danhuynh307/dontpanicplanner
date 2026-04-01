459a356a9aa370aef5b10a897511bef305f07f0f
hemang43 <hemanthrgoli@gmail.com>
2026-04-01 17:51:15 -0400

    Task ranking system by Hemanth G"

---
+++ src/main/java/com/example/dontpanicplanner/TaskRankSystem.java
@@ -0,0 +1,55 @@
+package com.example.dontpanicplanner;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskRankSystem {
+
+    private final PriorityScoreService scoreService;
+
+    // Constructor injection (instead of new)
+    public TaskRankSystem(PriorityScoreService scoreService) {
+        this.scoreService = scoreService;
+    }
+
+    /**
+     * Public method to rank tasks:
+     * Compute priority scores
+     * Sort tasks in descending order
+     */
+    public void rankTasks(TaskDataStructure<Task> tasks) {
+        applyScores(tasks);
+        sortByPriority(tasks);
+    }
+
+    /**
+    * Compute scores for all tasks
+     */
+    private void applyScores(TaskDataStructure<Task> tasks) {
+        for (int i = 0; i < tasks.size(); i++) {
+            Task t = tasks.get(i);
+            scoreService.applyPriorityScore(t);
+        }
+    }
+
+    /**
+     * Sort tasks
+     */
+    private void sortByPriority(TaskDataStructure<Task> tasks) {
+        for (int i = 0; i < tasks.size(); i++) {
+            for (int j = 0; j < tasks.size() - 1; j++) {
+
+                Task t1 = tasks.get(j);
+                Task t2 = tasks.get(j + 1);
+
+                int p1 = (t1.getPriorityScore() == null) ? 0 : t1.getPriorityScore();
+                int p2 = (t2.getPriorityScore() == null) ? 0 : t2.getPriorityScore();
+
+                if (p1 < p2) {
+                    tasks.set(j, t2);
+                    tasks.set(j + 1, t1);
+                }
+            }
+        }
+    }
+}
